40f8582334c5a1e03c99cde6e48a6143bf17bb4e
Batuhan Aksüt <batuhanaksut@hotmail.com>
2024-05-15 22:41:53 +0300

    watchtower problem first commit

---
+++ Watchtower/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("x?");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("y?");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (y > 0)
+{
+    if (x < 0)
+    {
+        Console.WriteLine("The enemy is to the northwest!");
+    }
+    else if (x == 0)
+    {
+        Console.WriteLine("The enemy is to the north!");
+    }
+    else
+    {
+        Console.WriteLine("The enemy is to the northeast!");
+    }
+}
+
+else if (y == 0)
+{
+    if (x < 0)
+    {
+        Console.WriteLine("The enemy is to the west!");
+    }
+    else if (x == 0)
+    {
+        Console.WriteLine("The enemy is here!");
+    }
+    else
+    {
+        Console.WriteLine("The enemy is to the east!");
+    }
+}
+
+
+else
+{
+    if (x < 0)
+    {
+        Console.WriteLine("The enemy is to the southwest!");
+    }
+    else if (x == 0)
+    {
+        Console.WriteLine("The enemy is south!");
+    }
+    else
+    {
+        Console.WriteLine("The enemy is to the southeast!");
+    }
+}
