e71ed9c757587a82900759fba5f618c52dae84c5
BatuAksut <batuhanaksut@hotmail.com>
2025-01-14 14:45:22 +0300

    Locked Door solved

---
+++ part1_the_basics/The Locked Door/Door.cs
@@ -0,0 +1,94 @@
+﻿public class Door
+{
+    public bool IsLocked { get; set; }
+    public bool IsOpen { get; set; }
+    public int Password { get; set; }
+
+    public Door(int passcode)
+    {
+        Password = passcode;
+        IsLocked = true;
+        IsOpen = false;
+    }
+
+    public void ChangePassword(int currentPassword, int newPassword)
+    {
+        if (currentPassword == Password)
+        {
+            Password = newPassword;
+            Console.WriteLine("Password changed successfully");
+        }
+        else
+        {
+            Console.WriteLine("Wrong password");
+        }
+    }
+
+    public void Lock()
+    {
+        if (!IsLocked && !IsOpen)
+        {
+            IsLocked = true;
+            Console.WriteLine("Door is locked");
+        }
+        else if (IsOpen)
+        {
+            Console.WriteLine("Door should be closed first");
+        }
+        else if (IsLocked)
+        {
+            Console.WriteLine("Door is already locked");
+        }
+    }
+
+    public void Unlock(int pass)
+    {
+        if (IsLocked && !IsOpen && pass == Password)
+        {
+            IsLocked = false;
+            Console.WriteLine("Door is unlocked");
+        }
+        else if (IsOpen)
+        {
+            Console.WriteLine("Door should be closed first");
+        }
+        else if (!IsLocked)
+        {
+            Console.WriteLine("Door is already unlocked");
+        }
+    }
+
+    public void Close()
+    {
+        if (!IsLocked && IsOpen)
+        {
+            IsOpen = false;
+            Console.WriteLine("Door is closed");
+        }
+        else if (!IsOpen)
+        {
+            Console.WriteLine("Door is already closed");
+        }
+        else if (IsLocked)
+        {
+            Console.WriteLine("Door is locked");
+        }
+    }
+
+    public void Open()
+    {
+        if (!IsLocked && !IsOpen)
+        {
+            IsOpen = true;
+            Console.WriteLine("Door is open");
+        }
+        else if (IsOpen)
+        {
+            Console.WriteLine("Door is already open");
+        }
+        else if (IsLocked)
+        {
+            Console.WriteLine("Door is locked");
+        }
+    }
+}
---
+++ part1_the_basics/The Locked Door/Program.cs
@@ -0,0 +1,57 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Define password");
+        var pass = Convert.ToInt32(Console.ReadLine());
+        Door myDoor = new Door(pass);
+
+        bool continueProgram = true;
+
+        while (continueProgram)
+        {
+            Console.WriteLine("\nChoose an option:");
+            Console.WriteLine("1. Lock the door");
+            Console.WriteLine("2. Unlock the door");
+            Console.WriteLine("3. Open the door");
+            Console.WriteLine("4. Close the door");
+            Console.WriteLine("5. Change password");
+            Console.WriteLine("6. Exit");
+            Console.Write("Your choice: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    myDoor.Lock();
+                    break;
+                case "2":
+                    Console.Write("Enter password to unlock: ");
+                    int unlockPassword = Convert.ToInt32(Console.ReadLine());
+                    myDoor.Unlock(unlockPassword);
+                    break;
+                case "3":
+                    myDoor.Open();
+                    break;
+                case "4":
+                    myDoor.Close();
+                    break;
+                case "5":
+                    Console.Write("Enter current password: ");
+                    int currentPassword = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter new password: ");
+                    int newPassword = Convert.ToInt32(Console.ReadLine());
+                    myDoor.ChangePassword(currentPassword, newPassword);
+                    break;
+                case "6":
+                    continueProgram = false;
+                    Console.WriteLine("Exiting...");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please choose again.");
+                    break;
+            }
+        }
+    }
+}
